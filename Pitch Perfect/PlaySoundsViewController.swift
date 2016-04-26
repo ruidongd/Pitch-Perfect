4138756fea843e6122d3dac8a4484f47ff41342f
ruidongd <ruidongd@outlook.com>
2016-04-26 12:38:20 -0700

    Add PlayBack
    Set tags for button
    Add the extension class for PlaySoundsViewControll
    
    Implement the playSoundForButton

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -20,12 +20,48 @@ class PlaySoundsViewController: UIViewController {
     @IBOutlet weak var rabbitButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     
+    var recordedAudioURL: NSURL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayNode: AVAudioPlayerNode!
+    var stopTimer: NSTimer!
+    
+    enum ButtonType: Int {case Slow = 0, Fast, Chipmunk, Vader, Echo, Reverb}
+    
+    
     @IBAction func playSoundForButton(sender: UIButton){
         print("Play Sound Button Pressed")
+        stopButton.enabled = true
+        switch(ButtonType(rawValue: sender.tag)!){
+        case .Slow:
+            playSound(rate: 0.5)
+        case .Fast:
+            playSound(rate: 1.5)
+        case .Chipmunk:
+            playSound(pitch: 1000)
+        case .Vader:
+            playSound(pitch: -1000)
+        case .Echo:
+            playSound(echo: true)
+        case .Reverb:
+            playSound(reverb: true)
+        }
+        configureUI(.Playing)
+        
     }
-    @IBAction func stopSoundPlay(sender: UIButton){
+    @IBAction func stopPressed(sender: UIButton){
         print("Stop Play Sound")
     }
     
-    var recordedAudioURL: NSURL!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAudio()
+        stopButton.enabled = false
+    }
+    override func viewWillAppear(animated: Bool) {
+        configureUI(.NotPlaying)
+        
+    }
 }
