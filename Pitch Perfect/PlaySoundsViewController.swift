b6f81821d005878393668366a5423c3422195b37
ruidongd <ruidongd@outlook.com>
2016-04-27 20:24:29 -0700

    Fix bugs of ChipmunkButton and VaderButton

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -12,10 +12,10 @@ import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     
-    @IBOutlet weak var echoButton: UIView!
+    @IBOutlet weak var echoButton: UIButton!
     @IBOutlet weak var reverbButton: UIButton!
     @IBOutlet weak var chipmunkButton: UIButton!
-    @IBOutlet weak var darthVaderButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
     @IBOutlet weak var snailButton: UIButton!
     @IBOutlet weak var rabbitButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
@@ -23,7 +23,7 @@ class PlaySoundsViewController: UIViewController {
     var recordedAudioURL: NSURL!
     var audioFile: AVAudioFile!
     var audioEngine: AVAudioEngine!
-    var audioPlayNode: AVAudioPlayerNode!
+    var audioPlayerNode: AVAudioPlayerNode!
     var stopTimer: NSTimer!
     
     enum ButtonType: Int {case Slow = 0, Fast, Chipmunk, Vader, Echo, Reverb}
